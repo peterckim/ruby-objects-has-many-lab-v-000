698492b9cfbd4c7dd9396c15528ca62a13d2e941
Peter Kim <peterckim96@gmail.com>
2018-06-08 21:09:58 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -6,8 +6,8 @@ class Author
     @posts = []
   end
   
-  def add_post(input)
-    @posts << input
-    input.author = self
+  def add_post(post)
+    @posts << post
+    post.author = self
   end
 end
