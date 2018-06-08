1e9a244bcd8002160e226e3ac4656445fef863aa
Peter Kim <peterckim96@gmail.com>
2018-06-08 21:12:17 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,16 +9,18 @@ class Author
   
   def add_post(post)
     @posts << post
+    @@all_posts << post
     post.author = self
   end
   
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
+    @@posts << post
     post.author = self
   end
   
   def self.post_count
-    
+    @@posts.length
   end
 end
