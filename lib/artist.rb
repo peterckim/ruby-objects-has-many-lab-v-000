a794ccbfcb78315320facd7cd9cc376833073d0c
Peter Kim <peterckim96@gmail.com>
2018-06-08 21:19:49 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@all_songs = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+    @@all_songs << song
+    song.artist = self
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    @@all_songs << song
+    song.artist = self
+  end
+  
+  def self.song_count
+    @@all_songs.length
+  end
+end
---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+class Author
+  attr_accessor :name, :posts
+  @@all_posts = []
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def add_post(post)
+    @posts << post
+    @@all_posts << post
+    post.author = self
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    @@all_posts << post
+    post.author = self
+  end
+  
+  def self.post_count
+    @@all_posts.length
+  end
+end
